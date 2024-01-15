7fac3e44c26fff597bb94e015c781a9d64e8f0b8
gaddiel <mbengigaddiel@outlook.fr>
2024-01-15 14:29:56 +0100

    last modificcation

--- Front-end/src/pages/HomePage.jsx
+++ Front-end/src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
+// HomePage.jsx
 import React from "react";
 import "../css/main.css";
 import homePageData from "../data/homePage.json";
+import HeroSection from "../components/Home/HeroSection";
+import FeaturesSection from "../components/Home/FeaturesSection";
 
 const HomePage = () => {
   const { heroContent, features } = homePageData;
@@ -8,33 +11,8 @@ const HomePage = () => {
   return (
     <>
       <main>
-        <div className="hero">
-          <section className="hero-content">
-            <h2 className="sr-only">Promoted Content</h2>
-            {heroContent.map((content, index) => (
-              <p key={index} className="subtitle">
-                {content}
-              </p>
-            ))}
-            <p className="text">
-              Open a savings account with Argent Bank today!
-            </p>
-          </section>
-        </div>
-        <section className="features">
-          <h2 className="sr-only">Features</h2>
-          {features.map((feature, index) => (
-            <div key={index} className="feature-item">
-              <img
-                src={feature.icon}
-                alt={feature.title + " Icon"}
-                className="feature-icon"
-              />
-              <h3 className="feature-item-title">{feature.title}</h3>
-              <p>{feature.description}</p>
-            </div>
-          ))}
-        </section>
+        <HeroSection heroContent={heroContent} />
+        <FeaturesSection features={features} />
       </main>
     </>
   );
